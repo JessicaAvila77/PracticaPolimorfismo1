d32a7f8b8f8b66810ca874f25f19193cb35cfa0d
JessicaAvila77 <jessica.avilaunah@gmail.com>
2024-08-09 18:39:47 -0600

    Corrigiendo ciclo while

--- Program.cs
+++ Program.cs
@@ -11,46 +11,52 @@ namespace PracticaPolimorfismo1
     {
         static void Main(string[] args)
         {
-            
+            int Opcion;
 
             //creando la lista de tipo figura
             List<Figura> figuraLista = new List<Figura>();
 
-            int Opcion = Menu();
 
             do
             {
-               
+                Opcion = Menu();
+
                 switch (Opcion)
                 {
                     case 1:
-
                         Console.WriteLine("");
                         Console.Write("Ingrese el largo del rectángulo: ");
                         double largo = double.Parse(Console.ReadLine());
                         Console.Write("Ingrese el ancho del rectángulo: ");
                         double ancho = double.Parse(Console.ReadLine());
 
+                        //se agrega un nuevo objeto de tipo rectangulo a la lista
                         figuraLista.Add(new Rectangulo(largo, ancho));
                         break;
 
 
                     case 2:
-
                         Console.WriteLine("");
                         Console.Write("Ingrese el radio del circulo: ");
                         double radio = double.Parse(Console.ReadLine());
 
+                        //se agrega un nuevo objeto de tipo circulo a la lista
                         figuraLista.Add(new Circulo(radio));
                         break;
 
                     case 3:
+                        Console.WriteLine("");
                         Console.Write("Ingrese la base del triangulo: ");
                         double baseTriangulo = double.Parse(Console.ReadLine());
                         Console.Write("Ingrese la altura del triangulo: ");
                         double altura = double.Parse(Console.ReadLine());
 
+                        //se agrega un nuevo objeto de tipo triangulo a la lista
                         figuraLista.Add(new Triangulo(baseTriangulo, altura));
+                        break;
+
+                    case 4:
+                        Console.WriteLine("Gracias por usar nuestros servicios");
                         break;
 
                     default:
@@ -60,20 +66,28 @@ namespace PracticaPolimorfismo1
 
                 }
 
-             Opcion = Menu();
+             //int Opcion = Menu();
 
 
             }while (Opcion != 4);
 
-            Console.WriteLine("");
-            Console.WriteLine("======Area de de las figuras ingresadas=====");
+            
 
             //llama al método imprimir por cada figura en la lista de tipo Figura
-            foreach (Figura figura in figuraLista)
+            if (figuraLista.Count > 0)
             {
-                figura.Imprimir();
-            }
+                Console.WriteLine("");
+                Console.WriteLine("======Area de de las figuras ingresadas=====");
+                foreach (Figura figura in figuraLista)
+                {
+                    figura.Imprimir();
+                }
 
+            } else
+            {
+                Console.WriteLine("Gracias por usar nuestro sistema");
+            }
+            
 
         }
 
